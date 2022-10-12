11c15505ca5632b40dd8606bee20bf21aa9e4b4a
jtiagosantos <tiagosan040@gmail.com>
2022-10-11 23:44:31 -0300

    feat: implement Text component

---
+++ src/components/Text/Text.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text } from './Text';
+
+//types
+import { TextProps } from './types';
+
+export default { //global definitions
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Lorem ipsum',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<TextProps>;
+
+//definitions by variation
+export const Default: StoryObj<TextProps> = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg',
+  },
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    size: 'md',
+    asChild: true,
+    children: (
+      <p>Lorem ipsum</p>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
