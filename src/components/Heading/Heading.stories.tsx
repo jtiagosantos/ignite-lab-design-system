1811f70b261f68b8a9183fe571ec8b68fcdf1cdb
jtiagosantos <tiagosan040@gmail.com>
2022-10-11 23:43:45 -0300

    feat: implement Heading component

---
+++ src/components/Heading/Heading.stories.tsx
@@ -0,0 +1,62 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Heading } from './Heading';
+
+//types
+import { HeadingProps } from './types';
+
+export default { 
+  title: 'Components/Heading',
+  component: Heading,
+  args: {
+    children: 'Lorem ipsum',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
+} as Meta<HeadingProps>;
+
+export const Default: StoryObj<HeadingProps> = {
+  args: {
+    size: 'md',
+  },
+};
+
+export const Small: StoryObj<HeadingProps> = {
+  args: {
+    size: 'sm',
+  },
+};
+
+export const Large: StoryObj<HeadingProps> = {
+  args: {
+    size: 'lg',
+  },
+};
+
+export const CustomComponent: StoryObj<HeadingProps> = {
+  args: {
+    size: 'md',
+    asChild: true,
+    children: (
+      <h1>Lorem ipsum</h1>
+    )
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      },
+    },
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+};
