50b53d24516abb8574b0f8bdf682db1df3f3332a
jtiagosantos <tiagosan040@gmail.com>
2022-10-11 23:43:31 -0300

    feat: implement Checkbox component

---
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text } from '../Text/Text';
+import { Checkbox } from './Checkbox';
+
+export default { 
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  decorators: [
+    (Story) => (
+      <div className='flex items-center gap-2'>
+        {Story()}
+        <Text size="sm">Lembrar de mim por 30 dias</Text>
+      </div>
+    )
+  ],
+} as Meta;
+
+export const Default: StoryObj = {};
+
